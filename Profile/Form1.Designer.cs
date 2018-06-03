3a7ec210f1c198b26e7e4a3445f220cbd30251c5
lakshang <lakshan.1998@gmail.com>
2018-06-03 21:34:43 +0530

    Changes to automatically set the image size according to the PictureBox. [SizeMode = StretchImage]

--- Profile/Form1.Designer.cs
+++ Profile/Form1.Designer.cs
@@ -95,6 +95,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(341, 27);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(357, 270);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
